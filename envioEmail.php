289e2ff5f0ab0711a356a0a8399d4145e1e4dccb
GuilhermeAFerrari <guile.ferrari@hotmail.com>
2020-06-05 09:52:38 -0300

    Fix error envioEmail

--- envioEmail.php
+++ envioEmail.php
@@ -12,12 +12,12 @@
  
 		// Método de envio
 		$mail->IsSMTP(); // Enviar por SMTP 
-		$mail->Host = "mail.bluewash.com.br"; // Você pode alterar este parametro para o endereço de SMTP do seu provedor
+		$mail->Host = ""; // Você pode alterar este parametro para o endereço de SMTP do seu provedor
 		$mail->Port = 587; 
  
 		$mail->SMTPAuth = true; // Usar autenticação SMTP (obrigatório)
-		$mail->Username = 'tecnico@bluewash.com.br'; // Usuário do servidor SMTP (endereço de email)
-		$mail->Password = 'Bw@102030'; // Mesma senha da sua conta de email
+		$mail->Username = ''; // Usuário do servidor SMTP (endereço de email)
+		$mail->Password = ''; // Mesma senha da sua conta de email
  
 		// Configurações de compatibilidade para autenticação em TLS
 		$mail->SMTPOptions = array(
@@ -30,11 +30,11 @@
 		// $mail->SMTPDebug = 2; // Você pode habilitar esta opção caso tenha problemas. Assim pode identificar mensagens de erro.
  
 		// Define o remetente
-		$mail->From = "tecnico@bluewash.com.br"; // Seu e-mail
+		$mail->From = ""; // Seu e-mail
 		$mail->FromName = "Recuperar Senha TecPlus"; // Seu nome
  
 		// Define o(s) destinatário(s)
-		$mail->AddAddress('inserir o email', 'Guilherme');
+		$mail->AddAddress('inserir o email', '');
  
 		// Definir se o e-mail é em formato HTML ou texto plano
 		$mail->IsHTML(true); // Formato HTML . Use "false" para enviar em formato texto simples.
